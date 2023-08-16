7477ce48a28c1fc9df357b3a4fe5d53d4e8ff47b
Lukies07 <luke.cillekens@sbhsstudent.school.nz>
2023-08-17 11:13:52 +1200

    added stuff

--- game.js
+++ game.js
@@ -8,32 +8,46 @@ canvas.height = 600;
 // Player properties
 const playerWidth = 50;
 const playerHeight = 50;
+//keep player inside the canvas
 let playerX = canvas.width / 2 - playerWidth / 2;
 const playerY = canvas.height - playerHeight - 10;
 const playerSpeed = 5;
 
+//variables for jumping
+let isJumping = false
 // Arrow key state
 let leftPressed = false;
 let rightPressed = false;
+let upPressed = false;
 
 // Event listeners for arrow key presses
 document.addEventListener("keydown", keyDownHandler);
 document.addEventListener("keyup", keyUpHandler);
 
+
 function keyDownHandler(event) {
+    //arrow key listners
     if (event.key === "Left" || event.key === "ArrowLeft") {
         leftPressed = true;
     } else if (event.key === "Right" || event.key === "ArrowRight") {
         rightPressed = true;
+    }   else if (event.key === "Up" || event.key === "ArrowUp"){
+        upPressed = true
     }
+    
 }
 
 function keyUpHandler(event) {
     if (event.key === "Left" || event.key === "ArrowLeft") {
         leftPressed = false;
-    } else if (event.key === "Right" || event.key === "ArrowRight") {
+    } 
+        else if (event.key === "Right" || event.key === "ArrowRight") {
         rightPressed = false;
     }
+        else if (event.key === "Up" || event.key === "ArrowUp") {
+        UpPressed = false;
+    }
+
 }
 
 // Update player position
@@ -43,6 +57,9 @@ function updatePlayerPosition() {
     }
     if (rightPressed && playerX < canvas.width - playerWidth) {
         playerX += playerSpeed;
+    }
+    if (upPressed && isJumping) {
+        playerY + 10;
     }
 }
 
