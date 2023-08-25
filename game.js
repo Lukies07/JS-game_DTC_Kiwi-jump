f9acde7b1f0cb3cdeb14d382e0b523591c720ae3
Lukies07 <luke.cillekens@sbhsstudent.school.nz>
2023-08-25 12:30:03 +1200

    restarted

--- game.js
+++ game.js
@@ -2,151 +2,42 @@ const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 
 // Set canvas dimensions
-canvas.width = 800;
-canvas.height = 600;
+canvas.width = 1080;
+canvas.height = 720;
 
-//branch genreation
-let branchXpos = 0;
-let branchYpos = 400;
-let brachXwidth = 100;
-let branchYwidth = 20;
+let player = {
+  x: canvas.width/2,
+  y: player.height - canvas.height,
+  width: 40,
+  height: 40
+};
 
+document.addEventListener("keydown", function(event) {
 
-// Player properties
-const playerWidth = 50;
-const playerHeight = 50;
-
-//keep player inside the canvas
-let playerX = canvas.width / 2 - playerWidth / 2;
-const playerSpeed = 5;
-const gravity = 0.75;
-const jumpStrength = -20; // I have Negative value for upward movement
-let groundHeight = 550;
-
-//player jump stats
-let playerY = groundHeight;
-let jump = false;
-let velocityY = 0;
-
-// Arrow key state
-let leftPressed = false;
-let rightPressed = false;
-let upPressed = false;
-//c stands for colour
-const black = pixelData[0];
-const pixelData = ctx.getImageData(playerX + playerWidth, playerY + playerHeight, 1, 1).data; //get data for edges around player
-console.log(`Color at (${cubeX + relativeX}, ${cubeY + relativeY}): B=${cBlack}`);
-    
-
-// Event listeners for arrow key presses
-document.addEventListener("keydown", keyDownHandler);
-document.addEventListener("keyup", keyUpHandler);
-
-
-function keyDownHandler(event) {
-    //arrow key listners
-    if (event.key === "Left" || event.key === "ArrowLeft") {
-        leftPressed = true;
-    } if (event.key === "Right" || event.key === "ArrowRight") {
-        rightPressed = true;
-    }    if (event.key === "ArrowUp" && !jump) {
-        jump = true;
-        velocityY = jumpStrength;
-      }
-    
-}
-
-function keyUpHandler(event) {
-    if (event.key === "Left" || event.key === "ArrowLeft") {
-        leftPressed = false;
-    } 
-        if (event.key === "Right" || event.key === "ArrowRight") {
-        rightPressed = false;
-    }
-        if (event.key === "Up" || event.key === "ArrowUp") {
-        upPressed = false;
-    }
-
-}
-
-
-// Update player position
-function updatePlayerPosition() {
-    if (leftPressed && playerX > 0) {
-        playerX -= playerSpeed;
-    }
-    if (rightPressed && playerX < canvas.width - playerWidth) {
-        playerX += playerSpeed;
-    }
-    
-    
-}
-
-function checkPlayerBranchCollision() {
-    const leftFootPixelColor = ctx.getImageData(playerX, playerY + playerHeight, 1, 1).data;
-    const rightFootPixelColor = ctx.getImageData(playerX + playerWidth, playerY + playerHeight, 1, 1).data;
-    
-    // Check if the player's feet hit the branch's color (assuming black is the branch color)
-    if (
-      (leftFootPixelColor[0] === 0 && leftFootPixelColor[1] === 0 && leftFootPixelColor[2] === 0) ||
-      (rightFootPixelColor[0] === 0 && rightFootPixelColor[1] === 0 && rightFootPixelColor[2] === 0)
-    ) {
-      // Handle collision with branch (e.g., stop player's vertical movement)
-      velocityY = 0; // Stop player's vertical movement
-      playerY = branchYpos - playerHeight; // Move player above the branch
-      jump = false; // Allow player to jump again
-    }
-  }
-  
-  // Update function
-  function update() {
-    velocityY += gravity;
-    playerY += velocityY;
-  
-    if (playerY >= groundHeight) {
-      playerY = groundHeight;
-      velocityY = 0;
-      jump = false;
-    }
-  
-    // Check for collisions with branch
-    checkPlayerBranchCollision();
-  }
-
-  
-function update() {
-    // Apply gravity to the player's vertical velocity
-    velocityY += gravity;
-    
-    // Update player's Y position based on velocity
-    playerY += velocityY;
-    
-    // Check if the player has hit the ground
-    if (playerY >= groundHeight) {
-      playerY = groundHeight;
-      velocityY = 0;
-      jump = false;
-    }
-    
-    // Update player's position on the screen
-    // Here you might update the player's position in your game's rendering system
-  }
-
-// Game loop
-function gameLoop() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+  console.log("Key pressed:", event.key);
+      
+      if (event.key === 'd') {
+    player.x += 5; 
    
-    updatePlayerPosition();//runs the function to update player 
+  }
+      if (event.key === 'a') {
+    player.x -= 5;
+      }
 
-    //make sticks for player to jump on
-    ctx.fillStyle = "black";
-    ctx.fillRect(branchXpos, branchYpos,brachXwidth, branchYwidth);
+draw(); 
+});
 
-    // Draw player
-    ctx.fillStyle = "blue";
-    ctx.fillRect(playerX, playerY, playerWidth, playerHeight);
-    update(); //for the jumping
-    requestAnimationFrame(gameLoop);
+// Function to clear the canvas and draw the player
+function draw() {
+  // Clear the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+  // Set player color
+  ctx.fillStyle = "blue";
+
+  // Draw the player
+  ctx.fillRect(player.x, player.y, player.width, player.height);
 }
 
-gameLoop();
+// Initial drawing
+draw();
